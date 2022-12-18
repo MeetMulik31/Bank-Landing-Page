28d787db336cc183b20b69ab4b64315588371496
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2022-12-18 10:39:22 +0530

    Bank Website✨

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./index.css";
+import styles from "./style";
+import {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Footer,
+  Hero,
+  Testimonials,
+  Stats,
+  CTA,
+  Clients,
+} from "./components";
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Testimonials from "./Testimonials";
+import Stats from "./Stats";
+import CTA from "./CTA";
+import Clients from "./Clients";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Footer,
+  Hero,
+  Testimonials,
+  Stats,
+  CTA,
+  Clients,
+};
